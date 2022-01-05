62c6c8e760958d7abf3c14ef4dfa7a2c1f45453a
chepe <chepe_jm10@hotmail.com>
2022-01-05 01:13:21 -0600

    intento de qr

--- app/src/main/java/com/example/nuevo/qr.java
+++ app/src/main/java/com/example/nuevo/qr.java
@@ -1,14 +1,21 @@
 package com.example.nuevo;
 
+import android.content.Context;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import com.google.zxing.MultiFormatWriter;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-
+import com.google.zxing.*;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 
 public class qr extends AppCompatActivity {
@@ -39,6 +46,25 @@ public class qr extends AppCompatActivity {
                 String sText = etInput.getText().toString().trim();
                 //inicializa multi forma excritura
                 MultiFormatWriter writer = new MultiFormatWriter();
+                try{
+                    //inicializar bit matriz
+                    BitMatrix matrix = writer.encode(sText, BarcodeFormat.QR_CODE
+                    ,350,350);
+                    //inicializar barcode encoder
+                    BarcodeEncoder encoder = new BarcodeEncoder();
+                    //inicializar bitmap
+                    Bitmap bitmap = encoder.createBitmap(matrix);
+                    //set bitmap en imagen view
+                    ivOutput.setImageBitmap(bitmap);
+                    //inicializar salid manager
+                    InputMethodManager manager = (InputMethodManager)  getSystemService(
+                            Context.INPUT_METHOD_SERVICE
+                    );
+                    //hide soft keyboard
+                    manager.hideSoftInputFromWindow(etInput.getApplicationWindowToken(),0);
+                }catch (WriterException e){
+                    e.printStackTrace();
+                }
             }
         });
     }
